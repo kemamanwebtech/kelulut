384b1ed3775d88833af57d9224bb07fc78d40275
Raf <mrafsyam@gmail.com>
2017-09-15 23:07:03 +0800

    rearrange reports document into report folder

--- kelulut-main/app/src/main/java/org/kemaman/kelulut_main/utils/AppConfig.java
+++ kelulut-main/app/src/main/java/org/kemaman/kelulut_main/utils/AppConfig.java
@@ -8,16 +8,28 @@ import android.content.Context;
 
 public class AppConfig {
 
-    public static String URL_LOGIN = "http://144.217.93.255/kelulut/login.php";
-    public static String URL_REGISTER = "http://144.217.93.255/kelulut/register.php";
-    public static String URL_GET_GENUS = "http://144.217.93.255/kelulut/get_genus_info.php";
-    public static String URL_GET_GENUS_SPECIES = "http://144.217.93.255/kelulut/get_genus_species.php";
-    public static String URL_UPLOAD = "http://144.217.93.255/kelulut/upload_image.php";
-    public static String URL_GETLOCATION = "http://144.217.93.255/kelulut/get_location.php";
-    public static String URL_GET_IMAGE = "http://144.217.93.255/kelulut/get_image.php";
-    public static String URL_GET_COMMENT = "http://144.217.93.255/kelulut/get_comment.php";
-    public static String URL_ADD_COMMENT = "http://144.217.93.255/kelulut/add_comment.php";
-    public static String URL_GET_QUESTION = "http://144.217.93.255/kelulut/get_question.php";
+//    public static String URL_LOGIN = "http://145.239.86.102/kelulut/login.php";
+//    public static String URL_REGISTER = "http://145.239.86.102/kelulut/register.php";
+//    public static String URL_GET_GENUS = "http://145.239.86.102/kelulut/get_genus_info.php";
+//    public static String URL_GET_GENUS_SPECIES = "http://145.239.86.102/kelulut/get_genus_species.php";
+//    public static String URL_UPLOAD = "http://145.239.86.102/kelulut/upload_image.php";
+//    public static String URL_GETLOCATION = "http://145.239.86.102/kelulut/get_location.php";
+//    public static String URL_GET_IMAGE = "http://145.239.86.102/kelulut/get_image.php";
+//    public static String URL_GET_COMMENT = "http://145.239.86.102/kelulut/get_comment.php";
+//    public static String URL_ADD_COMMENT = "http://145.239.86.102/kelulut/add_comment.php";
+//    public static String URL_GET_QUESTION = "http://145.239.86.102/kelulut/get_question.php";
+
+    public static String URL_LOGIN = "http://145.239.86.102:5000/login";
+    public static String URL_REGISTER = "http://145.239.86.102:5000/register";
+    public static String URL_GET_GENUS = "http://145.239.86.102:5000/get_genus_info";
+    public static String URL_GET_GENUS_SPECIES = "http://145.239.86.102:5000/get_genus_species";
+    public static String URL_UPLOAD = "http://145.239.86.102:5000/upload_image";
+    public static String URL_GETLOCATION = "http://145.239.86.102:5000/get_location";
+    public static String URL_GET_IMAGE = "http://145.239.86.102:5000/get_image";
+    public static String URL_GET_COMMENT = "http://145.239.86.102:5000/get_comment";
+    public static String URL_ADD_COMMENT = "http://145.239.86.102:5000/add_comment";
+    public static String URL_GET_QUESTION = "http://145.239.86.102:5000/get_question";
+    public static String URL_SAVE_SCORE = "http://145.239.86.102:5000/save_score";
 
     // For Shared Pref
     public static String SHAREDPREF_NAME = "KELULUT_SHAREDPREF";
