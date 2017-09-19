3d72ce1296bb559033873ec8d3f43ee0a6d17a6f
Raf <mrafsyam@gmail.com>
2017-09-19 21:53:53 +0800

    Add shutdown method

--- webservice/main.py
+++ webservice/main.py
@@ -143,8 +143,18 @@ def uploadImage():
     # TOOD perform analysis & update table uploaded_images here
     return result
 
+@app.route('/shutdown', methods=['POST', 'GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
 if __name__ == '__main__':
     app.run()
 
 
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
